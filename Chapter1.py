0a4e4260c97d8692183ec7b90e87cda3dbb2a67a
phanisai.g <phanisai.g@algonomy.com>
2025-06-12 11:03:02 +0530

    addedd about variable

---
+++ Chapter1.py
@@ -0,0 +1,8 @@
+# disussion abourt variable in python.
+'''
+1 python is case sensitive that means A variable will not be the same smalle letter a
+2 variable name should not start with number but it can be used in the middile.
+3 Reserved words cannot be used in variable name.
+4 _ when we use these underscore in the pythn variable name then its private.
+
+'''
