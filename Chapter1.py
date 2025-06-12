53b10f9afa2076acd28665df95d338502a4e576c
phanisai.g <phanisai.g@algonomy.com>
2025-06-12 11:13:38 +0530

    updated

--- Chapter1.py
+++ Chapter1.py
@@ -4,5 +4,4 @@
 2 variable name should not start with number but it can be used in the middile.
 3 Reserved words cannot be used in variable name.
 4 _ when we use these underscore in the pythn variable name then its private.
-
 '''
