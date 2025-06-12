cb62a9a2b06a1e24f5ec92bdb60dff333ad50be7
phanisai.g <phanisai.g@algonomy.com>
2025-06-12 11:27:19 +0530

    added about data types

---
+++ Chapter2.py
@@ -0,0 +1,16 @@
+'''
+in this we will be talking about the important concept in python that is data types.
+
+here we have inbuild data types in python are 
+INT :integer
+STR : string 
+BOOL: Boolean type
+float: flaoting data point means decimal point data type.
+
+# when we want to assign the varible to groupd of data then 
+list
+tuple
+set
+frozenset
+
+'''
