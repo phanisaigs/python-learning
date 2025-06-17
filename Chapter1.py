4b75634441511bf66d554a0c922cc61a5c69aa48
phanisai.g <phanisai.g@algonomy.com>
2025-06-17 19:03:24 +0530

    continue of python learning practise

--- Chapter1.py
+++ Chapter1.py
@@ -5,3 +5,28 @@
 3 Reserved words cannot be used in variable name.
 4 _ when we use these underscore in the pythn variable name then its private.
 '''
+
+phanisai =10
+Meena=30
+print (phanisai+Meena)
+
+# the above one simple example that has been used for varialbe along with operation concept.
+
+'''
+Data types in python
+1. int : int datatype represent the value of integer 0 to 9
+2. string : string data type we can represent the data in string that means words with double quotes or single quotes
+3. float : float data types that we can represent using flaot value like 3.1,5.1,8.4 like 
+4. Boolean : only two condition can apply to variable either the condition is TRUE or FALSE.
+5. complex: to complex calculation.
+6.bytes
+7. bytesarray
+8.range :
+9. list 
+10. tuple:
+11. set data type
+12. frozen set.
+13.dict
+14.None 
+
+'''
