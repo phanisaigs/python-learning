edac62ba2294250d26c2ae7885b8087bde16486a
phanisai.g <phanisai.g@algonomy.com>
2025-08-02 22:43:48 +0530

    syn the change to repo as well

--- advanced_data_types.py
+++ advanced_data_types.py
@@ -70,7 +70,7 @@ s1 = s*2
 print (s)
  
 # Tuple : Diffrence between list and tuple ius list data type is mutable annd tuple data type is immutable.
-# tuple can be represent using () brackets
+# tuple can be represent using () parenthesis.
 
 servers_list = ("lon-rt-101", "lon-rt-102", "lon-rt-103" ,"50.8" ,True)
 print(servers_list)
@@ -78,3 +78,80 @@ print(servers_list)
 '''
 the same logic will be applicable for tuple as well like list only once its updated canont be modified.
 '''  
+
+# Range data type: Range data type to represent the sequnce of values and its immutable.
+# diffreent type of form is avaialble when we use range data type 
+'''
+Form 1 : range(10), it represent the values 0 to 9
+Form2 : range (10, 99)
+example of forms
+Form1 : range (10) stop
+form2: range (start and stop)
+form3: range (start , step , stop)
+ 0 to end -1 
+
+phanisai = range(100)
+for i in phanisai: print(i)
+
+meena = range(110,200)
+print(meena[10])
+print(meena[15])
+print(meena[20])
+print(tuple(meena[10:20]))
+'''
+
+harsha = range(10,100,20)
+for i in harsha:print(i)
+
+
+# Set data type: Insertion order will not preserved and duplicates are not allowded then we should use for sert data type {} curly brases will be used to represent the set data type.
+phanisai = {10,20,30,10,20,30,}
+print(phanisai)
+
+# using the set data we can allow to add the duplicate collection but it will not print output.
+#phanisai = {10,20,30,10,20,30,}
+#print (phanisai[0])
+# accessing the elements using indexing or slicing operator does now allow in set data type.
+# insertion oder will not preserved.
+# duplicates are not allowed.
+# set data type is mutable that once we updated then we can add or remove the object from the collection.
+
+s=set()
+for i in range(100):
+    s.add(i)
+    print(s)
+
+# Forzen set.
+'''
+all the features are same like set data type only changes is the frozenset is immutable that mean we cant add or remove the element once we set.
+'''
+
+# dictionary:
+'''
+dict can be represent in key : value pair.
+key should be having unique value but value can be replaced.
+ex: dictionary or phone book.
+dict can be represent using {}
+key can be any type also value can be any type we can use in dict
+order will not preserved.
+'''
+phanisai = {100: "meena", 101: "harsha", 103:"sridevi"}
+print(phanisai)
+
+phani = {"phanisai":102, 20.32:"harsha.gp", 102:"canwe do this"}
+print(phani)
+
+d1={}
+print(d1)
+d1[100]="phanisai"
+print(d1)
+d1[101]=203.323232
+print(d1)
+d1[101]="harshagp"
+print(d1)
+
+# None data type.
+def f1():
+    a1 =10
+    print(a1)
+f1()
