5f7c0c4e7d7d4d0dadf83a0ec21c391101b8c0dd
phanisai.g <phanisai.g@algonomy.com>
2025-09-06 18:57:16 +0530

    Modifying based on structure

---
+++ Python/operators.py
@@ -0,0 +1,23 @@
+'''
+operators 
+
+Types of operator that are available in python.
+1. Arithmatic operator.
+2.Relational operator or comparision operator.
+3.Logical operator.
+4.Bitwise operator
+5.Assignemnet operator.
+Special operator.
+
+Arithmatic operator.
++, -, *, /, % // - floor division operator.
+** - Expenent operator.
+
+'''
+a = 10
+b = 20
+print(a + b)
+print (a - b )
+print ( a * b)
+print (a % b)
+print (a / b)
